b4d0870b488921e60b0f1e4f7c1ee9614fa9262d
matidsc <64502487+matidsc@users.noreply.github.com>
2020-08-28 23:25:53 -0300

    Cambio de solucion

--- Articulos/VMenuArticulos.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class VMenuArticulos
-    Private Sub btnAtras_Click(sender As Object, e As EventArgs) Handles btnAtras.Click
-        Me.Close()
-        VMenuPrincipal.Show()
-
-    End Sub
-End Class
--- Personal/VMenuPersonal.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class VMenuPersonal
-
-End Class
